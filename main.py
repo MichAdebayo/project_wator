3d2bc4c5633d66ac31a0d8ffbc9b5eba217b38eb
Antoine Delvoye <delvoyeadf@gmail.com>
2024-10-31 13:29:31 +0100

    add main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from Environment import *
 
-ma_planete = Environment(5,5)
-grid = ma_planete.init_grille()
-for line in grid:
-    print("  ".join(line))
-# ma_planete.afficher_grille()
+t= 0 
+while t < 3:
+    ma_planete = Environment(5,5)
+    grid = ma_planete.init_grille()
+    for line in grid:
+        print(line)
+    t +=1
+        # print("  ".join(line))
+    # ma_planete.afficher_grille()
 
