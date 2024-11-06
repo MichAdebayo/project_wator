2aea983c5e9b16766c87add917d11337215757c6
MichAdebayo <adebayomichael4@gmail.com>
2024-11-06 05:23:24 +0100

    added code comments and changed grid dimension

--- settings.py
+++ settings.py
@@ -1,5 +1,14 @@
-taux_occupation = 0.20
-nb_sharks = 0.3
-nb_tunas = 0.7
-largeur = 3
-longueur = 3
+# The proportion of the grid that will be occupied by fish and sharks
+occupation_rate = 0.20
+
+# The proportion of the total population that will be sharks
+number_of_sharks = 0.3
+
+# The proportion of the total population that will be tunas
+number_of_tunas = 0.7
+
+# The width of the ocean grid
+width = 30
+
+# The height of the ocean grid
+height = 30
