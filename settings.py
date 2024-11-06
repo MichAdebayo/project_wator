629eeef03a8559d4205bd540c6d67cc73694cf06
MichAdebayo <adebayomichael4@gmail.com>
2024-11-06 06:50:12 +0100

    updated width and height

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@
 occupation_rate = 0.20
 
 # The proportion of the total population that will be sharks
-number_of_sharks = 0.3
+number_of_sharks = 0.35
 
 # The proportion of the total population that will be tunas
-number_of_tunas = 0.7
+number_of_tunas = 0.65
 
 # The width of the ocean grid
 width = 30
